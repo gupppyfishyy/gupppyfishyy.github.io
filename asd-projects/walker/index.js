e5c117fbe6f63075982779ee8393f1db279529e8
gupppyfishyy <gupppyfishy@gmail.com>
2025-03-27 17:42:36 +0000

    broken walker3

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -29,6 +29,7 @@ function runProgram(){
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keyup', handleKeyUp);
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -41,6 +42,7 @@ function runProgram(){
   function newFrame() {
    repositionGameItem()
    redrawnGameItem()
+   wallCollision()
   }
   
   /* 
@@ -63,6 +65,17 @@ function runProgram(){
         walker.Yspeed = 5;
     }
 }
+function handleKeyUp(event) {
+  if (
+    event.which === KEY.LEFT ||
+    event.which === KEY.UP ||
+    event.which === KEY.RIGHT ||
+    event.which === KEY.DOWN
+  ) {
+    walker.Xspeed= 0;
+    walker.Yspeed= 0;
+  }
+}
   
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
@@ -75,7 +88,23 @@ function repositionGameItem () {
     $("#walker").css("left", walker.X)
     $("#walker").css("top", walker.Y)
   }
-
+function wallCollision ()
+var bWidth: $("#board").width()
+var bHeight: $("#board").height()
+if (walker.X < 0) {
+  walker.X = 0;
+  }
+  if (walker.X > bWidth) {
+  walker.X = bWidth;
+  }
+  if (walker.Y < 0) {
+  walker.Y = 0;
+  }
+  if (walker.Y > bHeight) {
+  walker.Y = bHeight;
+  }
+  }
+  
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
